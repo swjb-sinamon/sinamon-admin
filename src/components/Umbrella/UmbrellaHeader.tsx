0c488204d49b69f51fffb8ec2fa00decca6d617e
combbm <combbm@naver.com>
2020-11-01 23:08:04 +0900

    feat: add click eventhandler

--- src/components/Umbrella/UmbrellaHeader.tsx
+++ src/components/Umbrella/UmbrellaHeader.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { Heading1, Heading3 } from '../../atomics/Typography/Heading';
 import BlankLine from '../../utils/BlankLine';
 import { MediumButton } from '../../atomics/Button';
+import ButtonGroup from '../ButtonGroup';
 
-const UmbrellaHeader: React.FC = () => {
+interface UmbrellaHeaderProps {
+  readonly onQRClick?: React.MouseEventHandler;
+  readonly onManualClick?: React.MouseEventHandler;
+}
+
+const UmbrellaHeader: React.FC<UmbrellaHeaderProps> = ({ onQRClick, onManualClick }) => {
   return (
     <>
       <Heading1>우산 대여 · 반납</Heading1>
       <Heading3>대여해줄 우산을 선택하고 QR코드를 스캔합니다.</Heading3>
       <BlankLine gap={10} />
-      <MediumButton>반납신청</MediumButton>
+      <ButtonGroup>
+        <MediumButton width={120} onClick={onQRClick}>
+          반납신청 (QR)
+        </MediumButton>
+        <MediumButton width={120} onClick={onManualClick}>
+          반납신청 (수동)
+        </MediumButton>
+      </ButtonGroup>
     </>
   );
 };
