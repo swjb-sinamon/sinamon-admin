db954be28552e520b0aec7e6e01f258df5cb305a
combbm <combbm@naver.com>
2020-11-15 19:11:01 +0900

    feat: create UmbrellaWithRentalType

--- src/types/Umbrella.ts
+++ src/types/Umbrella.ts
@@ -3,3 +3,11 @@ export interface UmbrellaType {
   readonly status: 'good' | 'worse';
   readonly createdAt: Date;
 }
+
+export interface UmbrellaWithRentalType extends UmbrellaType {
+  readonly rental: {
+    readonly lender: string;
+    readonly expiryDate: Date;
+    readonly isExpire: string;
+  };
+}
