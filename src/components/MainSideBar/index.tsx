a08d10210997ba3b9f40eaa13a28178f4790f3fd
combbm <combbm@naver.com>
2020-11-16 15:43:01 +0900

    style: remove unused import code

--- src/components/MainSideBar/index.tsx
+++ src/components/MainSideBar/index.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faBook,
-  faKey,
-  faSchool,
-  faSignOutAlt,
-  faTshirt,
-  faUmbrella,
-  faUser
-} from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt, faTshirt, faUmbrella } from '@fortawesome/free-solid-svg-icons';
 import MainSideBarItem from './MainSideBarItem';
 import SCREEN_SIZE from '../../styles/screen-size';
 import MainTitleBar from '../MainTitleBar';
