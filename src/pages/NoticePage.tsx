72b1b5486061f50d224ea81c86e065ffad808813
DaegyeomHa <combbm@naver.com>
2021-01-30 15:15:03 +0900

    fix: newline 적용 방지를 위해 trim() 추가

--- src/pages/NoticePage.tsx
+++ src/pages/NoticePage.tsx
@@ -49,75 +49,75 @@ const ModNoticeForm = styled.div`
 
 const NoticePage: React.FC = () => {
   const [notice, setNotice] = useState<string>('');
-  
+
   useEffect(() => {
     Api.get('/notice').then((res) => {
       if (res.data && res.data.success) setNotice(res.data.data);
     });
   }, []);
-  
+
   const onModClick = async () => {
     if (notice.trim() === '') {
       showToast('내용을 채워주세요!', 'danger');
       return;
     }
-    
+
     try {
       await Api.put('/notice', {
-        notice
+        notice: notice.trim()
       });
       showToast('성공적으로 공지사항이 수정되었습니다!', 'success');
     } catch (e) {
       if (!e.response.data) return;
       const { success, error } = e.response.data;
       if (success || !error) return;
-      
+
       if (error === ErrorMessage.NO_PERMISSION) {
         showToast('관리자만 접근 가능한 페이지입니다.', 'warning');
       }
     }
   };
-  
+
   return (
     <>
       <Helmet>
         <title>공지사항 관리 - 수정과 관리자</title>
       </Helmet>
-      
+
       <MainSideBarContainer>
         <MainSideBar />
-        
+
         <StyledContent>
           <Heading1>공지사항 관리</Heading1>
           <Heading3>수정과 사이트의 공지사항을 관리 합니다.</Heading3>
-          
+
           <BlankLine gap={20} />
-          
+
           <Heading3>※ 줄바꿈은 \n을 사용해야 합니다.</Heading3>
           <Label>
             ex) A\nB <br />A<br />B
           </Label>
-          
+
           <BlankLine gap={10} />
-          
+
           <ModNoticeForm>
             <ModTextArea
-              placeholder='수정할 내용을 입력하세요'
+              placeholder="수정할 내용을 입력하세요"
               onChange={(e) => setNotice(e.target.value)}
               value={notice}
             />
-            
+
             <BlankLine gap={10} />
-            
+
             <MediumButton onClick={onModClick}>수정하기</MediumButton>
           </ModNoticeForm>
-          
+
           <BlankLine gap={20} />
-          
+
           <Heading2>미리보기 (적용화면)</Heading2>
           <Label>모바일 버전에서는 정확한 미리보기가 불가능 할 수 있습니다.</Label>
           <BlankLine gap={10} />
-          
+
           <NoticePreview notice={notice} />
         </StyledContent>
       </MainSideBarContainer>
